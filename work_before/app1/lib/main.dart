ad8aa3b571afaee685816d7e5e2a84d3525b3f01
Tanmay Singh <tanmaysinghsheoran3008@gmail.com>
2024-08-22 21:31:35 +0530

    working app first draft

--- work_before/app1/lib/main.dart
+++ work_before/app1/lib/main.dart
@@ -1,125 +1,188 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(fontSize: 18),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// Home screen displaying people list
+class HomeScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> people = [
+    {
+      'name': 'Rohan Sharma',
+      'age': 28,
+      'occupation': 'Marketing Specialist',
+    },
+    {
+      'name': 'Priya Menon',
+      'age': 34,
+      'occupation': 'Software Engineer',
+    },
+    {
+      'name': 'Anjali Deshmukh',
+      'age': 42,
+      'occupation': 'Graphic Designer',
+    },
+    {
+      'name': 'Rajiv Kapoor',
+      'age': 38,
+      'occupation': 'Financial Analyst',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Person List'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.all(12.0),
+            elevation: 4.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(
+                  people[index]['name'][0],
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+              ),
+              title: Text(people[index]['name']),
+              subtitle: Text(
+                  '${people[index]['occupation']}, Age: ${people[index]['age']}'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddressForm(person: people[index]),
+                  ),
+                );
+              },
             ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Address Form Screen (After Selecting a Person)
+class AddressForm extends StatefulWidget {
+  final Map<String, dynamic> person;
+
+  AddressForm({required this.person});
+
+  @override
+  _AddressFormState createState() => _AddressFormState();
+}
+
+class _AddressFormState extends State<AddressForm> {
+  String? selectedState;
+  String? selectedDistrict;
+  String? selectedConstituency;
+  String? selectedLanguage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Address of ${widget.person['name']}'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Address of ${widget.person['name']}',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Occupation: ${widget.person['occupation']}, Age: ${widget.person['age']}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'State'),
+              items: ['Bihar', 'Delhi', 'Maharashtra', 'Karnataka']
+                  .map((state) => DropdownMenuItem(
+                        value: state,
+                        child: Text(state),
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedState = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'District'),
+              items: ['District 1', 'District 2', 'District 3']
+                  .map((district) => DropdownMenuItem(
+                        value: district,
+                        child: Text(district),
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedDistrict = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'Assembly Constituency'),
+              items: ['Constituency 1', 'Constituency 2']
+                  .map((constituency) => DropdownMenuItem(
+                        value: constituency,
+                        child: Text(constituency),
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedConstituency = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'Language'),
+              items: ['English', 'Hindi']
+                  .map((language) => DropdownMenuItem(
+                        value: language,
+                        child: Text(language),
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedLanguage = value;
+                });
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
